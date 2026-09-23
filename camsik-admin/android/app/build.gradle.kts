b1b57580e934202b99d88e186028a5cbc39f05b1
adarshsachan9967-ops <adarshsachan9967@gmail.com>
2026-09-24 00:48:32 +0530

    build(android): upgrade compileSdk to 36 for Android 16 compatibility across all apps

--- camsik-admin/android/app/build.gradle.kts
+++ camsik-admin/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "in.camsik.admin"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- camsik-partner/android/app/build.gradle.kts
+++ camsik-partner/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "in.camsik.partner"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
